61adc71a9098a30ba3d734ace9cfede37adc91e3
Gughashree-M <gughashreem@gmail.com>
2025-09-08 14:59:12 +0530

    js practice

---
+++ js practice/projectguessno.html/practice.js/demo.js
@@ -0,0 +1,116 @@
+// var a=50
+// var b=60
+// console.log(a+b)
+
+// function functionnname()
+// {
+// console.log("hiii")
+// }
+// functionnname()
+
+// function Add()
+// {
+// var a=20
+// var b=60
+// console.log(a+b)
+// }
+// Add()
+
+// var factor="SK" 
+// var fplayer="virat" 
+// var fmovie="Remo"
+
+// function Favourite()
+// {
+// console.log("Favourite actor =" +factor)
+// console.log("Favourite player =" +fplayer)
+// console.log("Favourite movie =" +fmovie)
+
+// }
+// Favourite()
+// Favourite()
+
+// function Add(a,b)
+// {
+//     console.log (a+b)
+// }
+// Add(10,50)
+// function  area(lenght,bredth)
+// {
+// var a=lenght*bredth
+//     console.log("The Area is:"+a)
+
+// }k
+// area(20,40)
+
+// function add(a,b)
+// {
+// return(a+b)
+// }
+
+//  var total=add(20,34)
+// console.log(total)
+
+// var homework = false    
+// if (homework) {
+
+//     console.log("Great Job")
+// }
+// else {
+//     console.log("Finish Your homework before playing")
+// // }
+// console.log(!true)
+// var color = "yellow"
+// if (color == "red") {
+//     console.log("stop")
+// }
+// if (color == "yellow") {
+//     console.log("get ready")
+// }
+// if (color == "green") {
+//     console.log("go")
+// }
+
+// var season="autumn"
+
+// if(season=="summer")
+// {
+
+//     console.log("Enjoy the blooming flowers")
+
+// }
+// if(season=="spring")
+// {
+
+//     console.log("Have fun in the sun")
+    
+// }
+// if(season=="autumn")
+// {
+
+//     console.log("Admire the colorful leaves")
+    
+// }
+// if(season=="winder")
+// {
+
+//     console.log("Bundle up and stay warm")
+    
+// } 
+// for(count=1;count<=10;count=count+1)
+// {
+//     console.log(count+"*2="+count*2)
+// }
+
+// var a=Math.random()
+// console.log(Math.floor(a*8)+1)
+
+// var b= Math.floor(1.229)
+// console.log(b)
+
+//----------- Project guess the number------//
+
+
+
+
+
